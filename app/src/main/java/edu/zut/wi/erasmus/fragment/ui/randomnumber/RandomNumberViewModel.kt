3a084e319ea8866d5c9eb38dd8fe15bce7245cea
Radosław Maciaszczyk <rmaciaszczyk@wi.zut.edu.pl>
2021-07-29 13:58:18 +0200

    first approach to solution code

---
+++ app/src/main/java/edu/zut/wi/erasmus/fragment/ui/randomnumber/RandomNumberViewModel.kt
@@ -0,0 +1,22 @@
+package edu.zut.wi.erasmus.fragment.ui.randomnumber
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class RandomNumberViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    var text: LiveData<String> = _text
+
+    fun generateRandom() {
+        _text.postValue(Random.nextInt(0,100).toString());
+    }
+
+    }
+
